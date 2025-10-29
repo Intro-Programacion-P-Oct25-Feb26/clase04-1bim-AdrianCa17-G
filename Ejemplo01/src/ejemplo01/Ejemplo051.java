c8bd74a78621809bc2f8382ea131674c2167d8cd
AdrianCa17-G <adrian.steven.guaman.carrion@gmail.com>
2025-10-29 12:16:39 -0500

    septimo commit

---
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -0,0 +1,35 @@
+/*
+René Rolando
+
+Elizalde Solano
+
+1983
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo051 {
+    public static void main(String[] args) {
+        // Secuencia de escape \n y \t
+        String nombreEstudiante = "René Rolando";
+        String apellidoEstudiante = "Elizalde Solano";
+        int nacimiento = 1983;
+        
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
+                "\n"+ nacimiento); 
+        
+        // \t es un tabulador que da un salto de 4 espacios 
+        // System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);
+        // \n es un salto de linea que hace que un dato vaya automaticamente a la siguiente linea  
+        // System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);   
+    }
+}
